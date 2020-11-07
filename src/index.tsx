c12d0f5f7088d2b35f5e975db17a41d054ee8b81
SilWolf <silwolf1121@gmail.com>
2020-11-07 13:01:53 +0800

    feat: fix css breaking changes

--- src/index.tsx
+++ src/index.tsx
@@ -22,8 +22,8 @@ import { createBrowserHistory } from 'history';
 import { Router, Route, Switch, Redirect } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.css';
-import 'assets/scss/paper-dashboard.scss?v=1.2.0';
-import 'assets/demo/demo.css';
+import './assets/scss/paper-dashboard.scss?v=1.2.0';
+import './assets/demo/demo.css';
 import 'perfect-scrollbar/css/perfect-scrollbar.css';
 
 import AdminLayout from './layouts/Admin';
