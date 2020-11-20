cbd8d20b1fe2787860f17a3ec9eeb53256549cd8
SilWolf <silwolf1121@gmail.com>
2020-11-20 19:01:20 +0800

    feat: builder (pt.3)

--- src/views/CharacterBuilder.tsx
+++ src/views/CharacterBuilder.tsx
@@ -2,18 +2,26 @@ import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { Button, Form, FormGroup, FormText, Input, Label } from 'reactstrap'
 
-type FormData = {
-  name: string
-  nickname: string
-  gender: string
-  race: string
-  background: string
-}
-
 enum FormStep {
   BASIC = 0,
   BIO = 1,
   FAMILY = 11,
+}
+
+enum Gender {
+  Male = 'M',
+  Female = 'F',
+}
+
+type FormData = {
+  name: string
+  nickname: string
+  gender: Gender
+  race: string
+  background: string
+  age: number
+  grow: string
+  reason: string
 }
 
 const CharacterBuilder = () => {
@@ -47,7 +55,10 @@ const CharacterBuilder = () => {
 
         <FormGroup>
           <Label>性別</Label>
-          <Input type="select" name="gender" innerRef={register()} />
+          <Input type="select" name="gender" innerRef={register()}>
+            <option value={Gender.Male}>男／雄性</option>
+            <option value={Gender.Female}>女／雌性</option>
+          </Input>
           <FormText color="muted">
             儘管性別不會影響角色的能力，但也許會讓您更喜歡您的角色。
           </FormText>
@@ -65,13 +76,13 @@ const CharacterBuilder = () => {
           <Label>背景</Label>
           <Input type="select" name="background" innerRef={register()} />
           <FormText color="muted">
-            在角色決心冒險之前，您的角色是什麼身分呢？不同的背景會讓您的角色有不同的優勢。
+            在角色決心冒險時，您的角色是什麼身分呢？不同的背景會讓您的角色有不同的優勢。
           </FormText>
         </FormGroup>
 
         <FormGroup>
           <Button type="submit" color="success">
-            下一步
+            下一步: 冒險之前的故事
           </Button>
         </FormGroup>
       </Form>
@@ -79,17 +90,68 @@ const CharacterBuilder = () => {
   } else if (formStep === FormStep.BIO) {
     const character = {
       name: getValues('name'),
+      thirdPerson: getValues('gender') === Gender.Female ? '她' : '他',
+      background: getValues('background'),
     }
 
     content = (
       <Form>
         <h5>我們多聊聊有關 {character.name} 以前的故事吧？</h5>
+        <FormGroup>
+          <Label>年齡</Label>
+          <FormText>{character.name} 在當上冒險者時多大呢？</FormText>
+          <Input type="select" name="age" innerRef={register()}>
+            <option value={0}>剛成年</option>
+            <option value={1}>成年不久</option>
+          </Input>
+        </FormGroup>
 
         <FormGroup>
-          <Label> {character.name} </Label>
-          <Input type="select" name="background" innerRef={register()} />
+          <Label>成長</Label>
+          <FormText>
+            {character.name} 是在怎樣的環境下長大成人？又有什麼人物陪伴著
+            {character.thirdPerson}成長？最終{character.thirdPerson}
+            是怎樣成為一名
+            {character.background}的？
+          </FormText>
+          <Input type="textarea" name="grow" innerRef={register()} />
           <FormText color="muted">
-            在角色決心冒險之前，您的角色是什麼身分呢？不同的背景會讓您的角色有不同的優勢。
+            請描寫你認為一段對塑造{character.name}最關鍵的成長經歷。
+            根據角色的背景，每人的成長經歷都不一樣，而這段經歷會塑造他／她的個性、特質、價值觀等等。
+            <br />
+            平凡的角色可能會和父母及兄弟姐妹，在平凡的家庭下成長，並在成年之前到學校上學習冒險的知識，希望世界和平；
+            <br />
+            悲慘的角色也許經歷過家園被毀滅，無力掙扎、爆發潛能的時候。
+          </FormText>
+        </FormGroup>
+
+        <FormGroup>
+          <Label>冒險的理由</Label>
+          <FormText>
+            要冒險就意味著暴露在危險之中，是城市長大的人無法想像的辛苦事，挨餓、風吹雨打等等都是冒險的一部分。
+            <br />
+            足以讓 {character.name} 放棄舒適的生活，也要冒險的理由是什麼？推動
+            {character.thirdPerson}前進的信念和動力是什麼？
+          </FormText>
+          <Input type="textarea" name="reason" innerRef={register()} />
+          <FormText color="muted">
+            請描寫你認為一段對塑造{character.name}最關鍵的成長經歷。
+            根據角色的背景，每人的成長經歷都不一樣，而這段經歷會塑造他／她的個性、特質、價值觀等等。
+            <br />
+            平凡的角色可能會和父母及兄弟姐妹，在平凡的家庭下成長，並在成年之前到學校上學習冒險的知識，希望世界和平；
+            <br />
+            悲慘的角色也許經歷過家園被毀滅，無力掙扎、爆發潛能的時候。
+          </FormText>
+        </FormGroup>
+
+        <FormGroup>
+          <Label>目標</Label>
+          <FormText>試著用不多於20字總結 {character.name} 冒險的目標</FormText>
+          <Input type="textarea" name="objective" innerRef={register()} />
+          <FormText color="muted">
+            無論是誰，都會有目的才會去犯險，且往往十分簡單直觀、一說出口就能產生共嗚，且成為角色的魅力所在。
+            <br />
+            目標往往也跟角色故事脫不了關係的。
           </FormText>
         </FormGroup>
       </Form>
