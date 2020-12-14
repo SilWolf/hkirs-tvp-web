8c1d54a41b83bf5c306a3c72957f8d0fc44a1518
Dicky Chan <hinyiuchan@hk01.com>
2020-12-14 17:56:41 +0800

    chore: firebase config

--- gulpfile.js
+++ gulpfile.js
@@ -5,70 +5,70 @@ gulp.task('licenses', async function () {
   // this is to add Creative Tim licenses in the production mode for the minified js
   gulp
     .src('build/static/js/*chunk.js', { base: './' })
-    .pipe(
-      gap.prependText(`/*!
+    .pipe
+    //       gap.prependText(`/*!
 
-=========================================================
-* Paper Dashboard React - v1.2.0
-=========================================================
+    // =========================================================
+    // * Paper Dashboard React - v1.2.0
+    // =========================================================
 
-* Product Page: https://www.creative-tim.com/product/paper-dashboard-react
-* Copyright 2020 Creative Tim (http://www.creative-tim.com)
+    // * Product Page: https://www.creative-tim.com/product/paper-dashboard-react
+    // * Copyright 2020 Creative Tim (http://www.creative-tim.com)
 
-* Coded by Creative Tim
+    // * Coded by Creative Tim
 
-=========================================================
+    // =========================================================
 
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+    // * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
-*/`)
-    )
+    // */`)
+    ()
     .pipe(gulp.dest('./', { overwrite: true }))
 
   // this is to add Creative Tim licenses in the production mode for the minified html
   gulp
     .src('build/index.html', { base: './' })
-    .pipe(
-      gap.prependText(`<!--
+    .pipe
+    //       gap.prependText(`<!--
 
-=========================================================
-* Paper Dashboard React - v1.2.0
-=========================================================
+    // =========================================================
+    // * Paper Dashboard React - v1.2.0
+    // =========================================================
 
-* Product Page: https://www.creative-tim.com/product/paper-dashboard-react
-* Copyright 2020 Creative Tim (http://www.creative-tim.com)
+    // * Product Page: https://www.creative-tim.com/product/paper-dashboard-react
+    // * Copyright 2020 Creative Tim (http://www.creative-tim.com)
 
-* Coded by Creative Tim
+    // * Coded by Creative Tim
 
-=========================================================
+    // =========================================================
 
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+    // * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
--->`)
-    )
+    // -->`)
+    ()
     .pipe(gulp.dest('./', { overwrite: true }))
 
   // this is to add Creative Tim licenses in the production mode for the minified css
   gulp
     .src('build/static/css/*chunk.css', { base: './' })
-    .pipe(
-      gap.prependText(`/*!
+    .pipe
+    //       gap.prependText(`/*!
 
-=========================================================
-* Paper Dashboard React - v1.2.0
-=========================================================
+    // =========================================================
+    // * Paper Dashboard React - v1.2.0
+    // =========================================================
 
-* Product Page: https://www.creative-tim.com/product/paper-dashboard-react
-* Copyright 2020 Creative Tim (http://www.creative-tim.com)
+    // * Product Page: https://www.creative-tim.com/product/paper-dashboard-react
+    // * Copyright 2020 Creative Tim (http://www.creative-tim.com)
 
-* Coded by Creative Tim
+    // * Coded by Creative Tim
 
-=========================================================
+    // =========================================================
 
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+    // * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
-*/`)
-    )
+    // */`)
+    ()
     .pipe(gulp.dest('./', { overwrite: true }))
   return
 })
